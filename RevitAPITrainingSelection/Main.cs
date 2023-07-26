4999b8e5756924e46f8301660375a42909c39417
andreyzr43 <andreyzr43@yandex.ru>
2023-07-26 07:52:27 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Сообщение", "Тест");
+            return Result.Succeeded;
+        }
+    }
+}
