75ce07558159df7f7bec3f657fb4e514447c0e2a
andreyzr43 <andreyzr43@yandex.ru>
2023-07-26 17:42:04 +0300

    1

---
+++ RevitAPITrainingSelectionPoint/Main.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelectionPoint
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pickedPoint = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Выберете точку");
+            TaskDialog.Show("Point info", $"X: {pickedPoint.X}, Y: {pickedPoint.Y}, Z:{pickedPoint.Z}") ;
+
+            return Result.Succeeded;
+        }
+    }
+}
