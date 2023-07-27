bbb650204bb88bda217cd4f28cc17ca8b7666aa7
andreyzr43 <andreyzr43@yandex.ru>
2023-07-27 07:52:51 +0300

    2

---
+++ RevitAPITrainingSearchByConditions/Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSearchByConditions
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            ElementCategoryFilter windowsCategoryFiltet = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+            ElementClassFilter windowsInstancesFilter = new ElementClassFilter(typeof(FamilyInstance));
+
+            LogicalAndFilter windowsFilter = new LogicalAndFilter(windowsCategoryFiltet, windowsInstancesFilter);
+
+            var windows = new FilteredElementCollector(doc)
+                .WherePasses(windowsFilter)
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("Windows info", windows.Count.ToString());
+
+
+            return Result.Succeeded;
+        }
+    }
+}
